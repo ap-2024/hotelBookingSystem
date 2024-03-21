6a60acf7084bf312bbb046c76c1a4e462fc6bf66
ap-2024 <147934948+ap-2024@users.noreply.github.com>
2024-03-20 20:43:02 -0400

    Update hotel_booking_system.py

--- hotel_booking_system.py
+++ hotel_booking_system.py
@@ -6,7 +6,6 @@ customtkinter.set_default_color_theme("dark-blue")
 mainWindow = customtkinter.CTk()
 mainWindow.geometry("1000x700")
 mainWindow.title("Booking Service")
-book = customtkinter.CTkButton(mainWindow, text="Book a room", width=140, height=28, hover_color="
-                               #2057b0", command=bookRoomButton)
+book = customtkinter.CTkButton(mainWindow, text="Book a room", width=140, height=28, hover_color="#2057b0", command=bookRoomButton)
 book.pack()
 mainWindow.mainloop()

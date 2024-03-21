0fabcbd2ec0a347c2c840fbd7e8852f324083682
ap-2024 <147934948+ap-2024@users.noreply.github.com>
2024-03-20 20:42:12 -0400

    Add files via upload

---
+++ hotel_booking_system.py
@@ -0,0 +1,12 @@
+import customtkinter
+def bookRoomButton():
+    print("pressed")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+mainWindow = customtkinter.CTk()
+mainWindow.geometry("1000x700")
+mainWindow.title("Booking Service")
+book = customtkinter.CTkButton(mainWindow, text="Book a room", width=140, height=28, hover_color="
+                               #2057b0", command=bookRoomButton)
+book.pack()
+mainWindow.mainloop()

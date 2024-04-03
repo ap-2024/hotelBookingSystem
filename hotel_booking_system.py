ede5c4360d066575fd2e54dc0de445e51b335475
ap-2024 <147934948+ap-2024@users.noreply.github.com>
2024-04-02 22:31:41 -0400

    Update hotel_booking_system.py

--- hotel_booking_system.py
+++ hotel_booking_system.py
@@ -1,6 +1,11 @@
+import csv
 import customtkinter
 def bookRoomButton():
-    print("pressed")
+    with open('hotelBookingList.csv') as csvfile:
+        file = csv.reader(csvfile)
+        for value in file:
+            if value[1] == "open":
+                print("Room", value[0], "is", value[1]+".")                
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 mainWindow = customtkinter.CTk()
@@ -8,4 +13,5 @@ mainWindow.geometry("1000x700")
 mainWindow.title("Booking Service")
 book = customtkinter.CTkButton(mainWindow, text="Book a room", width=140, height=28, hover_color="#2057b0", command=bookRoomButton)
 book.pack()
+book.place(x=430, y=310)
 mainWindow.mainloop()
